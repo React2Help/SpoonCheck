dea239357e2e666b63ce685de8b7c11560b45c14
ChurchDuck1 <bchurch2@csus.edu>
2026-09-20 13:50:37 -0700

    linter issues

--- shared/src/commonMain/kotlin/dev/react2help/spooncheck/App.kt
+++ shared/src/commonMain/kotlin/dev/react2help/spooncheck/App.kt
@@ -5,11 +5,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import dev.react2help.spooncheck.repositories.InMemoryTaskRepository
 import dev.react2help.spooncheck.ui.SettingsScreenGen
 import dev.react2help.spooncheck.ui.TaskCreationScreenGen
@@ -31,11 +29,11 @@ fun App() {
 
                 // temp for demonstration. Eventually navigate from home page/setup day screen
                 LaunchedEffect(state.wasCancelled) {
-                    if (state.wasCancelled) navController.navigate("taskCreation?dateEnabled=false")
+                    if (state.wasCancelled) navController.navigate("taskCreationDateDisabled")
                 }
 
                 LaunchedEffect(state.wasSaved) {
-                    if (state.wasSaved) navController.navigate("taskCreation?dateEnabled=true")
+                    if (state.wasSaved) navController.navigate("taskCreationDateEnabled")
                 }
 
                 SettingsScreenGen(
@@ -44,17 +42,7 @@ fun App() {
                 )
             }
 
-            composable(
-                route = "taskCreation?dateEnabled={dateEnabled}",
-                arguments =
-                    listOf(
-                        navArgument("dateEnabled") {
-                            type = NavType.BoolType
-                            defaultValue = false
-                        }
-                    )
-            ) { backStackEntry ->
-                val dateEnabled = backStackEntry.arguments?.get("dateEnabled") as? Boolean ?: false
+            composable("taskCreationDateDisabled") {
                 val viewModel = viewModel { TaskCreationViewModel(InMemoryTaskRepository()) }
                 val state by viewModel.uiState.collectAsStateWithLifecycle()
 
@@ -69,7 +57,26 @@ fun App() {
                 TaskCreationScreenGen(
                     onAction = { action -> viewModel.onAction(action) },
                     state = state,
-                    isDateFieldEnabled = dateEnabled
+                    isDateFieldEnabled = false
+                )
+            }
+
+            composable("taskCreationDateEnabled") {
+                val viewModel = viewModel { TaskCreationViewModel(InMemoryTaskRepository()) }
+                val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+                LaunchedEffect(state.wasCancelled) {
+                    if (state.wasCancelled) navController.navigate("taskList")
+                }
+
+                LaunchedEffect(state.wasSaved) {
+                    if (state.wasSaved) navController.navigate("taskList")
+                }
+
+                TaskCreationScreenGen(
+                    onAction = { action -> viewModel.onAction(action) },
+                    state = state,
+                    isDateFieldEnabled = true
                 )
             }
 
